63b9892ff2080be538aece92efabaccf5b0e3563
RainDance74 <104539834+RainDance74@users.noreply.github.com>
2025-02-12 19:38:43 +0500

    Fix token creation

--- src/back/App/App/Services/Implementations/BearerProvider.cs
+++ src/back/App/App/Services/Implementations/BearerProvider.cs
@@ -17,7 +17,7 @@ public class BearerProvider : IBearerProvider
 
         var claims = new[] { new Claim(ClaimTypes.Name, name) };
         var credentials = new SigningCredentials(JwtShared.SecurityKey, SecurityAlgorithms.HmacSha256);
-        var token = new JwtSecurityToken("ExampleServer", "ExampleClients", claims, expires: DateTime.Now.AddSeconds(360), signingCredentials: credentials);
+        var token = new JwtSecurityToken("GrpcCheckServer", "GrpcCheckClient", claims, expires: DateTime.Now.AddSeconds(360), signingCredentials: credentials);
         return JwtShared.JwtTokenHandler.WriteToken(token);
     }
 }
